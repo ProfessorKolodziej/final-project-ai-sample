b228f60705f0ad43c615953f37634d707eda0738
Ashley Kolodziej <kolodz10@bu.edu>
2025-03-03 19:03:11 -0500

    Generative AI sample code

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,18 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+import { GoogleGenerativeAI } from "https://cdn.jsdelivr.net/npm/@google/generative-ai@0.23.0/+esm";
+
+// Important: get your own API key. Do not use mine.
+// You can do that here: https://aistudio.google.com/app/apikey
+const API_KEY = "AIzaSyAV-3hLuYJT984YqygzT8fXTb7wFcVwJoE";
+
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "Tell me a joke.";
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
