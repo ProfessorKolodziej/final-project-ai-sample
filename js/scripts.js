c53445ec83c736f4e62e3bf104743b84b5592a8a
Ashley Kolodziej <kolodz10@bu.edu>
2025-04-09 21:14:12 -0400

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,37 @@ const genAI = new GoogleGenerativeAI(API_KEY);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "Tell me a joke.";
+const petNameInput = document.querySelector('#petname');
+const generateButton = document.querySelector('#generate-story');
+const storySection = document.querySelector('#story-result');
 
-const result = await model.generateContent(prompt);
-console.log(result.response.text());
+async function generateStory() {
+	const prompt = `Generate a backstory for the pet named ${petNameInput.value}.`;
+	await model.generateContent(prompt)
+		.then(result => storySection.innerHTML = `<p>${result.response.text()}</p>`);
+}
+
+// ? is same as checking if generateButton is null
+generateButton?.addEventListener("click", generateStory);
+
+// Write some new code to generate a random pet
+
+const randomPetButton = document.querySelector('#random-pet');
+
+function getRandomArbitrary(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
+
+function showRandomPet() {
+	fetch("../js/pets.json")
+		.then(response => response.json())
+		.then(data => {
+			const totalPets = data.length;
+			const randomNumber = Math.round(getRandomArbitrary(0, totalPets - 1));
+			const randomPet = data[randomNumber];
+			storySection.innerHTML = `<em>${randomPet.name}</em> <img src=${randomPet.image} />`;
+		});
+}
+
+randomPetButton?.addEventListener("click", showRandomPet);
